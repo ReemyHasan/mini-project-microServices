dd293fad000f3e7cb34d1e222cdc58670fae8a7d
ReemyHasan <reemhasan2000831@gmail.com>
2023-02-08 15:05:57 +0300

    Initial commit

--- order-service/src/main/java/com/miniproject/orderservice/repository/OrderRepo.java
+++
@@ -1,7 +0,0 @@
-package com.miniproject.orderservice.repository;
-
-import com.miniproject.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepo extends JpaRepository<Order,Long>{
-}
