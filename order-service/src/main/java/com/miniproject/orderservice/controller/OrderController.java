b3894568214ca8553ea27b4fcf41f1b52b027678
ReemyHasan <reemhasan2000831@gmail.com>
2023-02-09 22:56:45 +0300

    commit-7 Circuit Breaker

--- order-service/src/main/java/com/miniproject/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/miniproject/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.miniproject.orderservice.controller;
 
 import com.miniproject.orderservice.dto.OrderRequest;
 import com.miniproject.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,9 +14,13 @@ public class OrderController {
     private final OrderService orderService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory",fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest){
         orderService.placeOrder(orderRequest);
         return "Order placed successfully";
+    }
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return "order operation is temporary unavailable, Please Try Again Later";
     }
 
 }
